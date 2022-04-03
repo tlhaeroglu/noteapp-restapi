480a081de26d1f76f4690a8cb63db80afae379d1
tlhaeroglu <talha44el@gmail.com>
2022-04-04 02:49:21 +0300

    fix cors policy problem

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const app = express();
 const mongoose = require('mongoose');
 const notesRouter = require('./routes/notes');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
+app.use(cors({
+    origin: "*",
+}))
 require('dotenv/config');
 const conectionString = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.dt3dr.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 
